07e119d483d9e04576615608f1761ea4c84cddd3
Taylor <tjaroguetech@gmail.com>
2021-02-08 20:33:47 -0500

    Finished csv seeding for CPU

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'CPU_UserBenchmarks.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+    t = Cpu.new
+    t.type = row['Type']
+    t.part_number = row['Part Number']
+    t.rank = row['Rank']
+    t.samples = row['Samples']
+    t.brand = row['Brand']
+    t.URL = row['URL']
+    t.model = row['Model']
+    t.save
+end
+
+puts "There are now #{Cpu.count} processors in the CPU table"
