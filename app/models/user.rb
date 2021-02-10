06420ba150204df2bb399ab2d8811d7aa22e9693
Taylor <tjaroguetech@gmail.com>
2021-02-09 19:40:07 -0500

    Added validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :computers
+    validates :username, :email, presence: true
+    validates :username, :email, uniqueness: true
 end
