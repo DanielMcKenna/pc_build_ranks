d11422e87970608ea56c7f478821b9b6ca5cc32a
Taylor <tjaroguetech@gmail.com>
2021-02-18 21:01:30 -0500

    Added custom routes for JWT tokens

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :computers
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/login', to: 'auth#login'
+  get '/auto_login', to: 'auth#auto_login'
+  get '/user_is_authed', to: 'auth#user_is_authed'
 end
